6dfab9fba26598589824ad8949dc43c38ffab68b
William Bout <hello@williambout.me>
2017-01-24 21:12:03 -0800

    :mute: and Update dependencies

--- app/js/index.js
+++ app/js/index.js
@@ -7,7 +7,6 @@ const loader = document.getElementById('loader');
 
 webview.addEventListener('dom-ready', () => {
   webview.insertCSS(fs.readFileSync(path.join(__dirname, '/css/main.css'), 'utf8'));
-  webview.openDevTools();
 });
 
 webview.addEventListener('new-window', (event) => {
