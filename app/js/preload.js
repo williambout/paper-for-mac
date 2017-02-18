36602c706bcefbfaaa519eba06c1c7fedeb2fac2
William Bout <hello@williambout.me>
2017-02-18 15:39:24 -0800

    Badge Notification

--- app/js/preload.js
+++ app/js/preload.js
@@ -1,4 +1,5 @@
 const {SpellCheckHandler, ContextMenuListener, ContextMenuBuilder} = require('electron-spellchecker');
+const {remote } = require('electron');
 
 window.spellCheckHandler = new SpellCheckHandler();
 setTimeout(() => window.spellCheckHandler.attachToInput(), 1000);
@@ -8,3 +9,24 @@ window.spellCheckHandler.autoUnloadDictionariesOnBlur();
 
 window.contextMenuBuilder = new ContextMenuBuilder(window.spellCheckHandler, null, true);
 window.contextMenuListener = new ContextMenuListener((info) => { window.contextMenuBuilder.showPopupMenu(info); });
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  const notificationIndicator = document.querySelectorAll('.hp-notifications-badge')[0];
+
+  setTimeout(() => {
+    setBadge(notificationIndicator.textContent);
+  }, 200);
+
+  notificationIndicator.addEventListener('DOMCharacterDataModified', function(event) {
+    setBadge(event.newValue);
+    if (event.prevValue < 1)
+      remote.app.dock.bounce('informational');
+  });
+
+  function setBadge(newValue) {
+    if (newValue !== '0')
+      remote.app.dock.setBadge(newValue);
+    else
+      remote.app.dock.setBadge('');
+  }
+});
